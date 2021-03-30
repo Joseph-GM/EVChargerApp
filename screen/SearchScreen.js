6a1edcdab4b190b9eeffb9aaeda80725d3987d0e
Joseph-GM <jkim.english75@gmail.com>
2021-03-30 22:27:14 +0900

    add search page

--- screen/SearchScreen.js
+++ screen/SearchScreen.js
@@ -1,14 +1,25 @@
 import React, {useState} from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, TextInput } from 'react-native'
 
 
 
 export default function SearchScreen({navigation}) {
-    const [poiData, setPoiData] = useState({});
+    const [text, setText] = useState('');
 
+    const _onChange = event => setText(event.nativeEvent.text);
+    
     return (
         <View>
             <Text>SearchScreen</Text>
+            <TextInput
+                style={{borderWidth: 1, padding: 10, fontSize: 20}}
+                placeholder="Input POI Name.."
+                onchage={_onChange}
+            />
+            <Button 
+                title="Submit"
+                onPress={() => console.log(text)}
+            />
             <Button 
                 title="Go to result"
                 onPress = {()=> navigation.navigate('SearchResult')}
